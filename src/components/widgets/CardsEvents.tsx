4e47a8abea6c5ac09b763a1a852d39953e043741
Anthonyzok521 <66536793+Anthonyzok521@users.noreply.github.com>
2024-10-13 17:36:11 -0400

    feat: responsive in events

--- src/components/widgets/CardsEvents.tsx
+++ src/components/widgets/CardsEvents.tsx
@@ -7,25 +7,25 @@ import { IconMapPin } from "@tabler/icons-react";
 const CardsEvents = ({ title, description, image, positionImage, datetime, location }: IEvents, key: string) => {
     const [open, setOpen] = useState<boolean>(false);
 
-    return <div key={`e-${key}`} className="flex overflow-hidden rounded-xl border border-gray-200 dark:border-slate-900 dark:shadow-black/30 shadow-lg min-h-20 max-h-44 lg:scale-75">
+    return <div key={`e-${key}`} className="flex max-[360px]:flex-col max-[360px]:flex-col-reverse overflow-hidden rounded-xl border border-gray-200 dark:border-slate-900 dark:shadow-black/30 shadow-lg min-h-20 max-[360px]:max-h-80 max-h-44 lg:scale-75">
         <div className='flex flex-col w-full max-md:z-10'>
             <span className="p-2 font-bold text-gray-600 text-sm">{datetime}</span>
-            <div className="flex flex-col justify-start w-44 overflow-auto max-md:h-full max-md:justify-between">
-                <h2 className={`max-md:hidden p-2 font-bold ${open ? '' : 'text-ellipsis whitespace-nowrap overflow-hidden'}`}>{title} asd ad as asda dasd</h2>
-                <span className={`max-md:hidden transition-all pl-2 ${open ? '' : 'text-ellipsis whitespace-nowrap overflow-hidden'} max-w-max`}>{description} asdasd asdad asdas dad asd asd ad</span>
-                <div className="hidden max-md:flex flex-col overflow-auto w-44">
+            <div className="flex flex-col justify-start min-[361px]:w-44 sm:max-lg:w-80 max-[360px]:w-full overflow-auto max-lg:h-full max-lg:justify-between">
+                <h2 className={`max-lg:hidden p-2 font-bold ${open ? '' : 'text-ellipsis whitespace-nowrap overflow-hidden'}`}>{title} asd ad as asda dasd</h2>
+                <span className={`max-lg:hidden transition-all pl-2 ${open ? '' : 'text-ellipsis whitespace-nowrap overflow-hidden'} max-w-max`}>{description} asdasd asdad asdas dad asd asd ad</span>
+                <div className="hidden max-lg:flex flex-col overflow-auto sm:max-lg:w-80 min-[361px]:w-44 max-[360px]:w-full">
                     <h2 className={`p-2 font-bold ${open ? '' : 'text-ellipsis whitespace-nowrap overflow-hidden'}`}>{title} asd ad as asda dasd</h2>
-                    <span className={`transition-all pl-2 ${open ? '' : 'text-ellipsis whitespace-nowrap overflow-hidden'} max-w-max`}>{description} asdasd asdad asdas dad asd asd ad</span>
+                    <span className={`transition-all pl-2 ${open ? '' : 'text-ellipsis whitespace-nowrap overflow-hidden'} max-w-max`}>{description} asdasd asdad asdas dad asd asd ad <span className="max-[360px]:relative hidden p-2 max-[360px]:flex items-center gap-1 max-[360px]:justify-end text-sm"><IconMapPin className="max-[360px]:absolute max-[360px]:size-4 max-[360px]:left-0" />{location}</span></span>
                 </div>
                 <span className="text-sm hover:bg-slate-300/30 rounded-md p-2 select-none max-w-max cursor-pointer" onClick={() => setOpen(!open)}>{open ? 'Mostrar menos' : 'Mostrar mas'}</span>
             </div>
-            <span className="hidden p-2 md:flex items-center gap-1"><IconMapPin />{location}</span>
+            <span className="hidden p-2 sm:flex items-center gap-1"><IconMapPin />{location}</span>
         </div>
-        <div className='overflow-hidden max-md:w-full max-md:flex max-md:flex-col max-md:justify-between'>
+        <div className='overflow-hidden max-lg:w-full max-lg:flex max-lg:flex-col max-lg:justify-between'>
             <Suspense fallback={<div>Loading</div>}>
-                <Image loading={'lazy'} className={`rounded-bl-md ${positionImage}`} width={650} height={340} alt={title} src={`${image}`} />
+                <Image loading={'lazy'} className={`rounded-bl-md max-[360px]:rounded-md ${positionImage}`} width={650} height={340} alt={title} src={`${image}`} />
             </Suspense>
-            <span className="max-md:relative hidden p-2 max-md:flex items-center gap-1 max-md:justify-end text-sm"><IconMapPin className="max-md:absolute max-md:size-4 max-md:left-0" />{location}</span>
+            <span className="max-lg:relative hidden p-2 max-lg:flex items-center gap-1 max-lg:justify-end text-sm"><IconMapPin className="max-lg:absolute max-lg:size-4 max-lg:left-0" />{location}</span>
         </div>
     </div>
 }
